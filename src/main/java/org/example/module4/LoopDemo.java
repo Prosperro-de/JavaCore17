1f176a672ffe63688ba8c19106b1035c7c747fba
Mykola Klushyn <mklushyn@atlassian.com>
2023-12-19 20:33:53 +0100

    add loop demo

---
+++ src/main/java/org/example/module4/LoopDemo.java
@@ -0,0 +1,111 @@
+package org.example.module4;
+
+import java.util.Scanner;
+
+public class LoopDemo {
+    public static void main(String[] args) { //.csv
+        String[] emails = {
+                "oleg@gmail.com,26",
+                "olena@gmail.com,4",
+                "mykola@gmail.com,10",
+                "stepan@gmail.com,6"
+        };
+//        for (int i = 0; i < emails.length; i++) {
+//            sendBirthdayEmail(emails[i]);
+//        }
+
+//        for (int i = 0; i < emails.length; i++) {
+//            String[] result = emails[i].split(","); // length = 2  ["oleg@gmail.com", "26"]
+//            int daysBeforeExpiration = Integer.parseInt(result[1]);
+//            if (daysBeforeExpiration < 7) {
+//                sendRenewalEmail(result[0]);
+//            }
+//        }
+
+        String[] students = {
+                "Oleg", "Natalia", "Mykola", "Dmytro"
+        };
+
+        for (int i = 0; i < students.length; i++) {
+            if (i % 2 != 0 ) {
+               continue;
+            }
+            System.out.println(students[i]);
+        }
+
+
+//        int studentNumber = findStudentIndex(students);
+//        System.out.println("studentNumber = " + studentNumber);
+
+//        int[][] intArray = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}};
+//
+//        for (int i = 0; i < intArray.length; i++) {
+//            for (int j = 0; j < intArray[i].length; j++) {
+//                System.out.println(intArray[i][j]);
+//            }
+//        }
+
+//        for (String email: emails) { //Iterator Iterable
+//            System.out.println(email);
+//        }
+
+//        login();
+    }
+
+    public static int findStudentIndex(String[] students) {
+        int result = 0;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals("Natalia")) {
+                result = i;
+            }
+        }
+        return result;
+    }
+
+    public static void login() {
+        String password = "qwerty";
+        Scanner scanner = new Scanner(System.in);
+        boolean isLogged = false;
+        int attempts = 3;
+
+        while (!isLogged && attempts > 0) { //true && true   true && false = false
+            System.out.println("Please log in");
+            String userPassword = scanner.nextLine();
+            if (password.equals(userPassword)) {
+                System.out.println("Welcome");
+                isLogged = true;
+            } else {
+//                continue;
+                System.out.println("Please try again");
+                attempts--;
+            }
+        }
+
+
+//        do {
+//            //true && true   true && false = false
+//            System.out.println("Please log in");
+//            String userPassword = scanner.nextLine();
+//            if (password.equals(userPassword)) {
+//                System.out.println("Welcome");
+//                isLogged = true;
+//            } else {
+//                System.out.println("Please try again");
+//                attempts--;
+//            }
+//        } while (false);
+
+
+    }
+
+    public static void sendBirthdayEmail(String email) {
+        System.out.println("Happy birthday email sent to " + email);
+    }
+
+    public static void sendRenewalEmail(String email) {
+        System.out.println("Please renew your subscription email sent to " + email);
+    }
+}
